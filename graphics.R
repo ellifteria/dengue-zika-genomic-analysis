331329f382a91f77ef0ba9aa7fb9f615d76a5d91
Elli Beres <ellifteria@gmail.com>
2022-12-04 19:20:42 -0600

    Create new changes

---
+++ graphics.R
@@ -0,0 +1,28 @@
+library(ggVennDiagram)
+library(ggplot2)
+library(ggvenn)
+
+data <- c(
+    dd = read.delim("gene-ontology/DENV_infected_downreg_gene_names.txt", header=FALSE),
+    du = read.delim("gene-ontology/DENV_infected_upreg_gene_names.txt", header=FALSE),
+    zd = read.delim("gene-ontology/ZIKV_infected_downreg_gene_names.txt", header=FALSE),
+    zu = read.delim("gene-ontology/ZIKV_infected_upreg_gene_names.txt", header=FALSE)
+)
+
+ggVennDiagram(data[c("dd.V1", "zd.V1")],
+    label_alpha = 0,
+    category.names = c("Dengue downregulated", "Zika downregulated"))
+ggVennDiagram(data[c("du.V1", "zu.V1")],
+    label_alpha = 0,
+    category.names = c("Dengue upregulated", "Zika upregulated"))
+ggVennDiagram(data[c("dd.V1", "zu.V1")],
+    label_alpha = 0,
+    category.names = c("Dengue downregulated", "Zika upregulated"))
+ggVennDiagram(data[c("du.V1", "zd.V1")],
+    label_alpha = 0,
+    category.names = c("Dengue upregulated", "Zika downregulated"))
+
+ggvenn(data[c("dd.V1", "zd.V1")], show_percentage = FALSE)
+ggvenn(data[c("du.V1", "zu.V1")], show_percentage = FALSE)
+ggvenn(data[c("du.V1", "zd.V1")], show_percentage = FALSE)
+ggvenn(data[c("dd.V1", "zu.V1")], show_percentage = FALSE)
